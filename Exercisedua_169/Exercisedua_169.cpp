f51e2f8212aab448df8c6a4ff8e860eb132610c8
Ryvqi <rifqikhabibi03@gmail.com>
2023-06-05 15:00:51 +0700

    aaa

--- Exercisedua_169/Exercisedua_169.cpp
+++ Exercisedua_169/Exercisedua_169.cpp
@@ -1,87 +1,53 @@
 #include <iostream>
 #include <vector>
-#include <string>
 using namespace std;
 
-class pengarang;
-class penerbit {
-private:
-    string nama_penerbit;
-    vector<pengarang*> daftar_pengarang;
+class Penerbit {
 public:
-    penerbit(const string& nama) : nama_penerbit(nama) {}
-    void tambah_pengarang(pengarang* pengarang) {
-        daftar_pengarang.push_back(pengarang);
-    }
-    void daftar_pengarang() {
-        cout << "Daftar pengarang pada penerbit\"" << nama_penerbit << "\":" << endl;
-        for (auto pengarang : daftar_pengarang) {
-            cout << pengarang->get_nama_pengarang() << endl;
-        }
-        cout << endl;
-    }
+	string nama;
+	vector<Pengarang*> yang_diikuti;
 };
 
-class buku {
-private:
-    string judul_buku;
+class Pengarang {
 public:
-    buku(const string& judul) : judul_buku(judul) {}
-    string get_judul_buku() {
-        return judul_buku;
-    }
+	string nama;
+	vector<Penerbit*> yang_diterbitkan;
 };
 
-class pengarang {
-private:
-    string nama_pengarang;
-    vector<buku*> daftar_buku;
+class Buku {
 public:
-    pengarang(const string& nama) : nama_pengarang(nama) {}
-    void tambah_buku(buku* buku) {
-        daftar_buku.push_back(buku);
-    }
-    string get_nama_pengarang() {
-        return nama_pengarang;
-    }
-    void daftar_buku() {
-        cout << "Daftar buku yang dikarang\"" << nama_pengarang << "\":" << endl;
-        for (auto buku : daftar_buku) {
-            cout << buku->get_judul_buku() << endl;
-        }
-        cout << endl;
-    }
+	string judulbuku;
 };
-
 
 int main() {
-    penerbit gama_press("Gama Press");
-    penerbit intan_pariwara("Intan Pariwara");
 
-    pengarang joko("Joko");
-    pengarang lia("Lia");
-    pengarang giga("Giga");
-    pengarang asroni("Asroni");
+	Penerbit* varPenerbit1 = new Penerbit();
+	varPenerbit1->nama = "Gama Press";
+	Penerbit* varPenerbit2 = new Penerbit();
+	varPenerbit2->nama = "Intan Patiwara";
 
-    buku fisika("Fisika");
-    buku algoritma("Algoritma");
-    buku basisdata("Basisdata");
-    buku dasar_pemrograman("Dasar Pemrograman");
-    buku matematika("Matematika");
-    buku multimedia_1("Multimedia1");
+	Pengarang* varPengarang1 = new Pengarang();
+	varPengarang1->nama = "Joko";
+	Pengarang* varPengarang2 = new Pengarang();
+	varPengarang2->nama = "Lia";
+	Pengarang* varPengarang3 = new Pengarang();
+	varPengarang3->nama = "Giga";
+	Pengarang* varPengarang4 = new Pengarang();
+	varPengarang4->nama = "Asroni";
 
-    gama_press.tambah_pengarang(&joko);
-    gama_press.tambah_pengarang(&lia);
-    gama_press.tambah_pengarang(&giga);
+	Buku* varBuku1 = new Buku();
+	varBuku1->judulbuku = "Fisika";
+	Buku* varBuku2 = new Buku();
+	varBuku2->judulbuku = "Algoritma";
+	Buku* varBuku3 = new Buku();
+	varBuku3->judulbuku = "Basisdata";
+	Buku* varBuku4 = new Buku();
+	varBuku4->judulbuku = "Daftar pemrograman";
+	Buku* varBuku5 = new Buku();
+	varBuku5->judulbuku = "Matematika";
+	Buku* varBuku6 = new Buku();
+	varBuku6->judulbuku = "Multimedia1";
 
-    intan_pariwara.tambah_pengarang(&asroni);
-    intan_pariwara.tambah_pengarang(&giga);
+	return 0;
 
-    joko.tambah_buku(&fisika);
-    joko.tambah_buku(&algoritma);
-    lia.tambah_buku(&basisdata);
-    asroni.tambah_buku(&dasar_pemrograman);
-
-    return 0;
 }
-
